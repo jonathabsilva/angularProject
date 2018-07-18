6e60ca5687e4e9472457367ee9d9edb9e7619f80
Jonatha Bizerra Silva <jonathab.silva@gmail.com>
2018-07-17 22:23:55 -0300

    Links feitos!

--- src/app/personagens/personagens.component.ts
+++ src/app/personagens/personagens.component.ts
@@ -16,25 +16,27 @@ export class PersonagensComponent implements OnInit {
 
   personagens: Personagem[]
   lista: ListaPersonagens
-  
+  a: number;
+
   constructor(private personagensService: PersonagensService) { }
 
   ngOnInit() {
     this.personagensService.listaPersonagens(API)
     .subscribe(lista => this.lista = lista);
     console.log(this.lista);
-  
+    this.a = 0;
   }
 
   proximo(){
     this.personagensService.listaPersonagens(this.lista.next)
     .subscribe(lista => this.lista = lista);
-    
+    this.a++;
   }
 
   voltar(){
     this.personagensService.listaPersonagens(this.lista.previous)
     .subscribe(lista => this.lista = lista);
+    this.a--;
   }
 
 }
